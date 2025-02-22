1f02ff49a07782abb4f38f33b70d17fd347f72e0
RCAbney <ryan@rcabney.com>
2025-02-22 18:53:45 -0500

    add dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		extend: {
 			fontFamily: {
@@ -9,14 +10,13 @@ export default {
 			},
 			colors: {
 				rcabney: '#6c8b6b',
-				primaryText: 'rgba(0,0,0,0.8)'
+				primaryText: 'rgba(0,0,0,0.8)',
+				darkHeading: '#94a3b8'
 			},
 			screens: {
 				'xs': '475px'
 			}
 		},
 	},
-	plugins: [
-		require('@tailwindcss/typography')
-	],
+	plugins: [],
 }
